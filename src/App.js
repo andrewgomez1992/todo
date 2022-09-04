03c256de0deebbde7fa250c402ed40cb4c2c4547
Andrew Gomez <drewgomez209@gmail.com>
2022-09-03 19:36:50 -0700

    SubmitTodoHandler finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css'
 
 // Importing Components
@@ -6,12 +6,21 @@ import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+
+  const [inputText, setInputText] = useState("")
+  const [todos, setTodos] = useState([])
+
   return (
     <div className="App">
       <header>
         <h1>Ari and Drew's Todos</h1>
       </header>
-      <Form />
+      <Form 
+        todos={todos} 
+        setTodos={setTodos} 
+        inputText={inputText} 
+        setInputText={setInputText} 
+      />
       <TodoList />
     </div>
   );
