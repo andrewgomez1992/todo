78f592faf925b4aeea350bf0624cd1a6857877b9
Andrew Gomez <drewgomez209@gmail.com>
2022-09-03 19:08:03 -0700

    styles set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import './App.css'
 
+// Importing Components
+import Form from "./components/Form";
+
 function App() {
   return (
     <div className="App">
-      <h1>Todos</h1>
+      <header>
+        <h1>Ari and Drew's Todos</h1>
+      </header>
+      <Form />
     </div>
   );
 }
